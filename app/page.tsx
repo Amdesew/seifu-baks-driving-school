19cfb2199ad70ac2fef91bb81a88b7854fc77134
Amdesew <96917631+Amdesew@users.noreply.github.com>
2024-08-07 18:18:45 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 "use client"
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/globals.css';
+
 export default function Home() {
   async function getDjangoAPIData() {
     const response = await fetch("http://127.0.0.1:8000/api/oil/");
