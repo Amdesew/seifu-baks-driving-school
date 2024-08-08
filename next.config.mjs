633156b9a72d64dd25269958dd0ef1dcee4cbd70
Amdesew <frankamde77@gmail.com>
2024-08-08 14:34:38 +0300

    fixing next.config.mjs bug

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = {
-    trailingSlash: true, // or false, depending on your needs
-  };
-
-
 export default nextConfig;
